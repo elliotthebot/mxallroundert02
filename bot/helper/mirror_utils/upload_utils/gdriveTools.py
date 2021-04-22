d148761ee106c514c349427a42eccbf9e398627a
elliotthebot <81969654+elliotthebot@users.noreply.github.com>
2021-04-23 02:42:09 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -478,8 +478,8 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'TTU Mirror Bot Search',
-                                 author_name='TTU Mirror Bot',
+                                 title = 'Infinitex Mirror Bot Search',
+                                 author_name='infinitex Mirror Bot',
                                  author_url='https://github.com',
                                  html_content=content)
         return
@@ -551,9 +551,9 @@ class GoogleDriveHelper:
 
             for content in self.telegraph_content :
                 self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'TTU Cloud Mirror Bot Search',
-                                                        author_name='TTU Cloud Mirror Bot',
-                                                        author_url='https://t.me',
+                                                        title = 'Infinitex Mirror Bot Search',
+                                                        author_name='Infinitex Mirror Bot',
+                                                        author_url='https://t.me/infinitexy',
                                                         html_content=content
                                                         )['path'])
 
